3fc370ad096a891b6d60963017ebb3da3f731cab
rushikesh.deshapande <adm.rdeshpan@global.un.org>
2020-05-21 22:37:35 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -100,6 +101,6 @@ app.get('*', (req, res) => {
         name: 'Rushi'
     })
 })
-app.listen(3000, () => {
-    console.log('Express server listen at port 3000')
+app.listen(port, () => {
+    console.log('Express server listen at port '+port)
 })
